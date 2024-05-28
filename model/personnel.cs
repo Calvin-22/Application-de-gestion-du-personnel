0a749aeb7af966d83ffdc91dc0322f52b77f238c
Calvin-22 <CAL.EDITIONPRO@gmail.com>
2024-05-28 11:52:43 +0200

    Création package Dal, package bddmanager. Rattachement de la base de données avec le projet sous Visual Studio. Création des classes métiers correspondant aux tables et de la chaîne de connexion.

---
+++ model/personnel.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Application_de_gestion_du_personnel.model
+{
+    /// <summary>
+    /// Classe métier interne pour mémoriser les informations du personnel
+    /// </summary>
+    public class personnel
+    {
+        public int idpersonnel { get; }
+        public string nom { get; }
+        public string prenom { get; }
+        public int tel { get; }
+        public string mail { get; }
+        public int idservice { get; }
+
+        /// <summary>
+        /// Valorise les propriétés
+        /// </summary>
+        /// <param name="idpersonnel"></param>
+        /// <param name="nom"></param>
+        ///<param name="prenom"></param>
+        ///<param name="tel"></param>
+        ///<param name="mail"></param>
+        /// <param name="idservice"></param>
+        public personnel(int idpersonnel, string nom, string prenom, int tel, string mail, int idservice)
+        {
+            this.idpersonnel = idpersonnel;
+            this.nom = nom;
+            this.prenom = prenom;
+            this.tel = tel;
+            this.mail = mail;
+            this.idservice = idservice;
+        }
+    }
+}
---
+++ model/responsable.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Application_de_gestion_du_personnel.model
+{
+    /// <summary>
+    /// Classe métier interne pour mémoriser les informations d'authentification
+    /// </summary>
+    public class responsable
+    {
+        public string identifiant { get; }
+        public string Pwd { get; }
+
+        /// <summary>
+        /// Valorise les propriétés
+        /// </summary>
+        /// <param name="identifiant"></param>
+        /// <param name="pwd"></param>
+        public responsable(string identifiant, string pwd)
+        {
+            this.identifiant = identifiant;
+            this.Pwd = pwd;
+        }
+    }
+}
